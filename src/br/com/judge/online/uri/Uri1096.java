989a65a14a8ad480d5ec125a7118449653b185b8
Igor <igorleon06@gmail.com>
2022-04-14 23:21:22 -0300

    Uri1096 fixed

---
+++ src/br/com/judge/online/uri/Uri1096.java
@@ -0,0 +1,22 @@
+package br.com.judge.online.uri;
+
+public class Uri1096 {
+
+	public static void main(String[] args) {
+
+		
+		int valor1;
+		int valor2;
+		
+		for (valor1 = 1; valor1 <= 9; valor1+=2) {
+			for (valor2 = 7; valor2 >= 5; valor2--) {
+				System.out.println("I=" + valor1 + " J=" + valor2);
+			}
+		}
+			
+		
+		
+			
+		}
+
+	}
